79b21044e6c5e65fbbfff3e9ab3df5232b188f80
jaewon0225 <jaewonla0225@gmail.com>
2024-07-21 18:36:40 +0100

    Linting

--- example/example.cpp
+++ example/example.cpp
@@ -1,9 +1,9 @@
 #include "slam.hpp"
 
 int main() {
-    PoseGraph::PoseGraph graph;
-    graph.importEdges("../data/INTEL_P_toro_edges.graph");
-    graph.importPoses("../data/INTEL_P_toro_vertex.graph");
-    graph.optimise(5);
-    std::cout << "done" << std::endl;
+  PoseGraph::PoseGraph graph;
+  graph.importEdges("../data/INTEL_P_toro_edges.graph");
+  graph.importPoses("../data/INTEL_P_toro_vertex.graph");
+  graph.optimise(5);
+  std::cout << "done" << std::endl;
 }
